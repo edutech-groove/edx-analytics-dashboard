c06e2bcfef2bdc72b5dc6f8f0615c7e80319acf9
Thin Nguyen <thin.nguyen@groovetechnology.com>
2021-06-30 16:19:55 +0700

    - Remove settings lms_theme_url - Using fec component instead

--- analytics_dashboard/core/context_processors.py
+++ analytics_dashboard/core/context_processors.py
@@ -8,6 +8,5 @@ def common(_request):
         'platform_name': settings.PLATFORM_NAME,
         'application_name': settings.APPLICATION_NAME,
         'footer_links': settings.FOOTER_LINKS,
-        'lms_theme_url': settings.LMS_THEME_URL,
         'enhenced_theme': settings.ENHENCED_THEME,
     }
