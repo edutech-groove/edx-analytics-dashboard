216f8cbafd058eb7f35f724bcf8218b13ff5f830
Thin Nguyen <thin.nguyen@groovetechnology.com>
2021-07-05 16:02:03 +0700

    Update paths in templates

--- analytics_dashboard/core/templatetags/dashboard_extras.py
+++ analytics_dashboard/core/templatetags/dashboard_extras.py
@@ -121,19 +121,6 @@ def unicode_slugify(value):
 
 
 @register.filter
-def escape_json(data):
-    """
-    Escape a JSON string (or convert a dict to a JSON string, and then
-    escape it) for being embedded within an HTML template.
-    """
-    json_string = json.dumps(data) if isinstance(data, dict) else data
-    json_string = json_string.replace("&", "\\u0026")
-    json_string = json_string.replace(">", "\\u003e")
-    json_string = json_string.replace("<", "\\u003c")
-    return mark_safe(json_string)
-
-
-@register.filter
 def languade_code_to_cldr(language_code):
     """
     Returns language codes in the CLDR expected naming convention.  The CLDR
